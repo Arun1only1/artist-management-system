ef600e5c3078464a72ac3ca16cedef6beb028435
arun <arun1nly1@gmail.com>
2025-01-01 12:27:20 +0545

    feat: authentication + authorization guards added

---
+++ backend/src/decorators/permission.decorator.ts
@@ -0,0 +1,7 @@
+// import { SetMetadata } from '@nestjs/common';
+// import { Permission } from 'src/roles/dto/input/create.role.dto';
+
+// export const Permissions = (permissions: Permission[]) =>
+//   SetMetadata('permissions', permissions);
+
+// // @Permissions([])
